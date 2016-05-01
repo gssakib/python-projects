9566512cf5aa051b75a655bc6ae359b54d51f1c7
sakibgazi9@gmail.com <sakibgazi9@gmail.com>
2016-04-30 19:29:54 -0700

    Initializing new python project

---
+++ ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 -25.0 * 3.0 % 4.0
+print "Now I will count the eggs:"
+
+print (((3.0 + 2.0 + 1.0 - 5.0) + (4.0 % 2.0)) -1.0) / (4.0 + 6.0)
+
+print "is it true that 3 + 2 < 5 - 7?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print " what is 3 + 2?", 3.0 + 2.0
+print "what is 5 - 7", 5.0 - 7.0
+
+print "Oh, that why it's False"
+
+print "how about some more"
+
+print "Is is greater?", 5.0 > - 2.0
+print "Is it greater or equal?", 5.0>= -2.0
+
+print "is it less oe euqal ?", 5.0 <= -2.0
+
+print 100.0 - (25.0 * 3.0) % 4.0
+
+
+ 
