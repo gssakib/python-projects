1ef3a2545022b2f14fd7d6eab2876338da6edbd2
sakibgazi9@gmail.com <sakibgazi9@gmail.com>
2016-05-02 20:23:00 -0700

    finished ex8 && ex9. Initializing ex10

---
+++ ex9.py
@@ -0,0 +1,18 @@
+days = "Mon Tue Wed Thu Fri Sat Sun % s" % 're'
+months = "\n Jan\n Feb \n Mar \n Apr \n May \n Jun \n Jul \n Aug \n%r" % 'leap'
+
+
+
+
+print "Here are the days: %s " % days
+print "Here are the months :", months
+
+print """
+	There's something going on here.
+	With the three double-quotes.
+	We will be able to type as much as we like.
+	Even 4 lines if we wanr, or 5, or 6.
+	"""
+	
+	
+	
