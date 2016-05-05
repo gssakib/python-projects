4f1b6484f18f666464c1378bcfd224ee621c7146
sakibgazi9@gmail.com <sakibgazi9@gmail.com>
2016-05-04 21:24:13 -0700

    Finished ex 10. added some formatting info. Will initialize ex 11 tomm

--- ex10.py
+++ ex10.py
@@ -1,13 +1,28 @@
-tabby_cat = "\tI'm tabbed in."
+tabby_cat = "\t\"I'm tabbed in.\""
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
 	I'll do a list:
-	\t* Cat food
+	\t \v* Cat food
 	\t* Fishies
-	\t* Catnip\n\t        * Grass
+	\t* Catnip \n\t\t* Grass
 """
+
+
+des = "\"Gazi K\""
+
+name = "\"amanda\""
+age = 13
+eye = "round"
+eye_color = "brown"
+
+
+print "Her name is %s and she has %s eyes\n" % (name, eye + eye_color )
+
+print "\tHis name is %s" % des
+
+
 
 print tabby_cat
 print persian_cat
