e102cf2c9ad193e86284f4bddef152d7db46071d
sakibgazi9@gmail.com <sakibgazi9@gmail.com>
2016-05-07 05:21:44 -0700

    Fixed ex13.py. Will initialize ex 14 soon.

--- ex13.py
+++ ex13.py
@@ -3,20 +3,20 @@ script , b_1, b_2 = argv
 cnt = 1
 while cnt > 0:
 
-	if int(b_1) == 1 & int(b_2) == 0:
+	if int(b_1) == 1 and int(b_2) == 0:
 		no_1 = raw_input("Please enter first number:  ")
 		no_2 = raw_input("Please enter second number:  ")
-		reslt = no_1 + no_2
+		reslt = int(no_1) + int(no_2)
 		cnt = 0
-	elif int(b_1) == 0 & int(b_2) == 1:
+	elif int(b_1) == 0 and int(b_2) == 1:
 		no_1 = raw_input("Please enter first number:  ")
 		no_2 = raw_input("Please enter second number:  ")
-		reslt = no_1 * no_2
+		reslt = int(no_1) * int(no_2)
 		cnt = 0
 	else:
 		print("Please try to input correctly again ")
 		
+		
 
-
-print ("The final result is:  %r ") % reslt
+print ("The final result is:  %r  ") % reslt
 
